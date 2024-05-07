b581f806c2ecb1ace319a94393ed979336933eb8
104vishnu <vishnubaraili1704025@gmail.com>
2024-05-07 17:12:00 +0530

    2nd committ

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import Register from "./pages/register/Register";
 import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
-import CardView from "./components/card/CardView.jsx";
+
 import LearnNumbers from "./pages/learnnumbers/LearnNumbers.jsx";
 import Numbers from "./pages/numbers/Numbers.jsx";
 import Tamil from "./pages/languages/Tamil.jsx";
--- src/pages/activity/Activity.jsx
+++ src/pages/activity/Activity.jsx
@@ -1,37 +1,3 @@
-// // import React from 'react';
-// // import { Link } from 'react-router-dom';
-// // import './Activity.scss';
-
-// // function Activity() {
-// //   return (
-// //     <main className="main">
-// //       <div className="container">
-// //         <section className="activities">
-            
-// //           <Link to='/count-numbers'>
-// //             <div className="activity">
-// //               <img src="./img/countnumbers.png" alt="Count Numbers" />
-// //               <h3>Count Numbers</h3>
-// //               <p>Learn counting numbers with fun activities.</p>
-// //             </div>
-// //           </Link>
-
-// //           <Link to='/audio-to-number'>
-// //             <div className="activity">
-// //               <img src="./img/audiotonumbers.png" alt="Audio to Number" />
-// //               <h3>Audio to Number</h3>
-// //               <p>Listen to audio clips and identify the corresponding numbers.</p>
-// //             </div>
-// //           </Link>
-          
-// //         </section>
-// //       </div>
-// //     </main>
-// //   );
-// // }
-
-// // export default Activity;
-
 // import React from 'react';
 // import { Link } from 'react-router-dom';
 // import './Activity.scss';
@@ -44,7 +10,7 @@
             
 //           <Link to='/count-numbers'>
 //             <div className="activity">
-//               <img src="./img/countnumbers.gif" alt="Count Numbers" />
+//               <img src="./img/countnumbers.png" alt="Count Numbers" />
 //               <h3>Count Numbers</h3>
 //               <p>Learn counting numbers with fun activities.</p>
 //             </div>
@@ -52,25 +18,9 @@
 
 //           <Link to='/audio-to-number'>
 //             <div className="activity">
-//               <img src="./img/audiotonumbers.gif" alt="Audio to Number" />
+//               <img src="./img/audiotonumbers.png" alt="Audio to Number" />
 //               <h3>Audio to Number</h3>
 //               <p>Listen to audio clips and identify the corresponding numbers.</p>
-//             </div>
-//           </Link>
-
-//           <Link to='/subtraction'>
-//             <div className="activity">
-//               <img src="./img/subtraction.gif" alt="Subtraction" />
-//               <h3>Subtraction</h3>
-//               <p>Practice subtraction with interactive quizzes.</p>
-//             </div>
-//           </Link>
-
-//           <Link to='/addition'>
-//             <div className="activity">
-//               <img src="./img/additions.gif" alt="Addition" />
-//               <h3>Addition</h3>
-//               <p>Test your addition skills with engaging exercises.</p>
 //             </div>
 //           </Link>
           
@@ -81,3 +31,53 @@
 // }
 
 // export default Activity;
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Activity.scss';
+
+function Activity() {
+  return (
+    <main className="main">
+      <div className="container">
+        <section className="activities">
+            
+          <Link to='/count-numbers'>
+            <div className="activity">
+              <img src="./img/countnumbers.gif" alt="Count Numbers" />
+              <h3>Count Numbers</h3>
+              <p>Learn counting numbers with fun activities.</p>
+            </div>
+          </Link>
+
+          <Link to='/audio-to-number'>
+            <div className="activity">
+              <img src="./img/audiotonumbers.gif" alt="Audio to Number" />
+              <h3>Audio to Number</h3>
+              <p>Listen to audio clips and identify the corresponding numbers.</p>
+            </div>
+          </Link>
+
+          <Link to='/subtraction'>
+            <div className="activity">
+              <img src="./img/subtraction.gif" alt="Subtraction" />
+              <h3>Subtraction</h3>
+              <p>Practice subtraction with interactive quizzes.</p>
+            </div>
+          </Link>
+
+          <Link to='/addition'>
+            <div className="activity">
+              <img src="./img/additions.gif" alt="Addition" />
+              <h3>Addition</h3>
+              <p>Test your addition skills with engaging exercises.</p>
+            </div>
+          </Link>
+          
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default Activity;
